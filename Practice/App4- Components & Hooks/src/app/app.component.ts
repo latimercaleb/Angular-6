e5461a2263f5d229b0db74fa179191e751fde8c0
latimercaleb <nohitokaze@gmail.com>
2019-09-26 02:37:31 -0400

    Completed app 4

--- Practice/App4- Components & Hooks/src/app/app.component.ts
+++ Practice/App4- Components & Hooks/src/app/app.component.ts
@@ -6,4 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  numericValue: number;
+  timeStamp: string;
+  oddCollection: {numericValue: number, timeStamp: string}[] = [];
+  evenCollection: {numericValue: number, timeStamp: string}[] =[];
+
+  onGameTick(gameTick: {tickVal: number, timeOfTick: string}){
+    this.timeStamp = gameTick.timeOfTick;
+    this.numericValue = gameTick.tickVal;
+    // debugger;
+    this.evenOrOdd();
+  }
+
+  evenOrOdd(){
+    (this.numericValue % 2) == 0 ? this.insertEven() : this.insertOdd();
+  }
+
+  insertOdd(){
+    this.oddCollection.push({
+      numericValue : this.numericValue,
+      timeStamp: this.timeStamp
+    });
+  }
+
+  insertEven(){
+    this.evenCollection.push({
+      numericValue : this.numericValue,
+      timeStamp: this.timeStamp
+    });
+  }
 }
--- Practice/App4- Components & Hooks/src/app/game-control/game-control.component.ts
+++ Practice/App4- Components & Hooks/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
-header : string = "App 4: Components & Hooks"
+  header : string = "Game Control";
   constructor() { }
-
+  counter: number = 0;
+  counterID: any;
+  dateTime: string;
+  @Output('outputTick') newTick: EventEmitter = new EventEmitter<{tickVal: number, timeOfTick: string}>();
   ngOnInit() {
+  }
+
+   startGame(){
+    this.counterID = setInterval(this.tick.bind(this),1000,this.counter);
+  }
+
+  endGame(){
+    console.log('Game End');
+    clearInterval(this.counterID);
+  }
+
+  tick(){
+    this.counter ++;
+    this.dateTime = new Date().toLocaleTimeString('en-US');
+    this.newTick.emit({
+      tickVal: this.counter,
+      timeOfTick: this.dateTime
+    });
   }
 
 }
