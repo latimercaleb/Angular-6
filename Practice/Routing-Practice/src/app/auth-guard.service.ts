c232be34dcb6aa0832f212050375c51535501a9c
latimeks <clatimer@revspringinc.com>
2019-10-01 17:09:12 -0400

    Complete routing stub

--- Practice/Routing-Practice/src/app/auth-guard.service.ts
+++ Practice/Routing-Practice/src/app/auth-guard.service.ts
@@ -1,10 +1,10 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from "@angular/router";
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanActivateChild } from "@angular/router";
 import { Observable } from "rxjs";
 import { Injectable } from "@angular/core";
 import { AuthService } from "./auth.service";
 
 @Injectable()
-export class AuthGuard implements CanActivate{
+export class AuthGuard implements CanActivate, CanActivateChild{
     // To be a route guard it has to have CanActivate method
     constructor(private authService: AuthService, private router: Router){}
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
@@ -19,4 +19,13 @@ export class AuthGuard implements CanActivate{
             }
         )
     }
+
+    // For route guarding on children routes only use canActivateChild, it works the same way so it's easy to just delegate and inject the dependencies 
+
+    canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean{
+        return this.canActivate(route,state);
+    }
+
+
+
 }
--- Practice/Routing-Practice/src/app/home/home.component.ts
+++ Practice/Routing-Practice/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private routerInstance: Router) { }
+  constructor(private routerInstance: Router, private authService: AuthService) { }
 
   ngOnInit() {
   }
@@ -22,6 +23,16 @@ export class HomeComponent implements OnInit {
   onLoadServer(id: number){
     this.routerInstance.navigate(['/servers',id,'edit']);
     // this.routerInstance.navigate(['/servers',id,'edit'], {queryParams: {allowEdit: '1'}, fragment: 'Loading...'}); //  Adding query params and fragments
+  }
+
+  onLogin(){
+    this.authService.login();
+    console.log('Logged in');
+  }
+
+  onLogout(){
+    this.authService.logout();
+    console.log('Logged out');
   }
 
 }
