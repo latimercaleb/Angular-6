e350977481b780ca5e3f2abefa8b8cb3f93ecb10
latimercaleb <nohitokaze@gmail.com>
2019-10-21 22:52:55 -0400

    Adding complete functionality for app practice, working on first animation

--- Practice/App6-Forms-Template Driven/src/app/app.component.ts
+++ Practice/App6-Forms-Template Driven/src/app/app.component.ts
@@ -9,13 +9,27 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('appForm', {static: false}) myForm: NgForm;
   serviceTypes: string[] = [];
+  formSubmitted: boolean;
+  data: {email: string, sub: string, pass: string} = {
+    email: '',
+    sub:'',
+    pass:''
+  };
 
   constructor(){
     this.serviceTypes.push('Basic', 'Advanced', 'Pro');
+    this.formSubmitted = false;
   }
 
   handleSubmit(){
     console.log(this.myForm);
+    if(this.myForm.valid){
+      this.data.email = this.myForm.value.email;
+      this.data.sub = this.myForm.value.subscription;
+      this.data.pass = this.myForm.value.password;
+      this.myForm.reset({subscription: this.serviceTypes[1]});
+      this.formSubmitted = true;
+    }
   }
 
 }
