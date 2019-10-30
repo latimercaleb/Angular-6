d108b29fe91421bd65f9f92b80534e0a6503d67c
latimercaleb <nohitokaze@gmail.com>
2019-10-30 02:44:33 -0400

    Almost done need some custom validation logic next

--- Practice/App7-Forms-Reactive/src/app/app.component.ts
+++ Practice/App7-Forms-Reactive/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appForm: FormGroup;
+  statusCollection: String[];
+
+  ngOnInit(){
+    this.appForm = new FormGroup({
+      'projectName': new FormControl(null,Validators.required),
+      'email': new FormControl(null,[Validators.required, Validators.email]),
+      'status': new FormControl('Stable')
+    });
+    this.statusCollection = ['Stable','Critcal','Finished'];
+  }
+
+  onSubmit(){
+    console.log(this.appForm);
+  }
 }
