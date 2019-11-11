479a5283880065d06512604deafdb47e90ba5af7
latimeks <clatimer@revspringinc.com>
2019-11-11 17:34:50 -0500

    ng class added for button behaviour, need to do some internals on the sorting

--- Practice/App8-Pipes/src/app/sort.pipe.ts
+++ Practice/App8-Pipes/src/app/sort.pipe.ts
@@ -5,8 +5,23 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
   // Sort by sever.name, if sorting is activated
-  transform(value: any, ...args: any[]): any {
-    return null;
+  transform(collection: any, flag:boolean): any {
+    if(flag){
+      let t = [];
+      // Implement sorting method correctly
+     collection.sort((first,next) => {
+        if (first.name.localeCompare(next.name) <= 0 ){
+          t.push(first);
+        }else{
+          t.push(next);
+        }
+      })
+      console.log(t);
+      return collection;
+    }else{
+      return collection;
+    }
+    
   }
 
 }
