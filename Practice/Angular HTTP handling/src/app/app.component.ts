281f2be2724ba721222345015b92fd70f3677a4b
latimercaleb <nohitokaze@gmail.com>
2019-11-12 14:43:25 -0500

    The basic POST & GET

--- Practice/Angular HTTP handling/src/app/app.component.ts
+++ Practice/Angular HTTP handling/src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { NgForm } from '@angular/forms';
-import { post } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +12,30 @@ export class AppComponent implements OnInit {
   @ViewChild('postForm', {static: false}) postForm: NgForm;
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getPosts();
+  }
 
+  private getPosts(){
+    this.http.get('https://angular-practice-be.firebaseio.com/posts.json').subscribe(
+      (responseData) => {
+        console.log('Fetched data:');
+        console.log(responseData);
+      });
+  }
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(`Sent post of : ${postData.title} & ${postData.content}`);
+    this.http.post('https://angular-practice-be.firebaseio.com/posts.json', postData).subscribe( responseData => {
+      console.log(responseData);
+    });
     this.postForm.resetForm();
   }
 
   onFetchPosts() {
     // Send Http request
+    this.getPosts();
+
   }
 
   onClearPosts() {
