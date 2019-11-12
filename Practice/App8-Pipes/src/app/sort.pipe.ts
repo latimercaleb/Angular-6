bb1ed0298f40ee15d7543adfab0b76678ac70433
latimercaleb <nohitokaze@gmail.com>
2019-11-12 00:33:16 -0500

    Completing app 8

--- Practice/App8-Pipes/src/app/sort.pipe.ts
+++ Practice/App8-Pipes/src/app/sort.pipe.ts
@@ -2,21 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'sort',
+  pure: false
 })
 export class SortPipe implements PipeTransform {
   // Sort by sever.name, if sorting is activated
   transform(collection: any, flag:boolean): any {
     if(flag){
-      let t = [];
-      // Implement sorting method correctly
-     collection.sort((first,next) => {
-        if (first.name.localeCompare(next.name) <= 0 ){
-          t.push(first);
-        }else{
-          t.push(next);
-        }
-      })
-      console.log(t);
+      collection.sort((a,b)=>{
+        return a.name > b.name ? 1 : -1;
+      });
       return collection;
     }else{
       return collection;
