654419025d6253592f9a2561bf579bf4cafabcac
latimeks <clatimer@revspringinc.com>
2019-11-14 15:20:58 -0500

    Interceptors: Response Modifications

--- Practice/Angular HTTP handling/src/app/auth-interceptor.service.ts
+++ Practice/Angular HTTP handling/src/app/auth-interceptor.service.ts
@@ -1,4 +1,5 @@
-import { HttpInterceptor, HttpRequest, HttpHandler} from '@angular/common/http';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEventType} from '@angular/common/http';
+import { tap } from 'rxjs/operators';
 
 export class AuthInterceptorService implements HttpInterceptor{
     // Runs a method on the incoming request, then calls a method to send the request back to it's path
@@ -6,6 +7,14 @@ export class AuthInterceptorService implements HttpInterceptor{
         console.log(req);
         console.log('Outbound Request intercepted!');
         const newRq = req.clone({headers: req.headers.append('TestingNewHeader', 'Falcon Armor')});
-        return next.handle(newRq);
+        return next.handle(newRq).pipe(tap(
+            evt => {
+                console.log(evt);
+                if (evt.type === HttpEventType.Response){
+                    console.log('It\'s a response!');
+                    console.log(evt.body);
+                }
+            }
+        ));
     }
 }
