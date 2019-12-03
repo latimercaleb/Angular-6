6c5cf3246478eabbfdfc8062f2c3cc33a3529c24
latimercaleb <nohitokaze@gmail.com>
2019-12-03 06:19:42 -0500

    Animating another div with more complex steps

--- Practice/Angular-Animations/src/app/app.component.ts
+++ Practice/Angular-Animations/src/app/app.component.ts
@@ -15,13 +15,38 @@ import {trigger, state, style, transition, animate} from '@angular/animations';
         'background-color': 'chartreuse',
         transform: 'translateX(100px)'
       })),
-      transition("default => highlighted", animate(500)),
-      transition("highlighted => default", animate(1500)),
+    transition("default <=> highlighted", animate(500))
+    ]),    
+    trigger('secondForm',[
+      state('default', style({
+        backgroundColor: 'teal',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'chartreuse',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'aliceblue',
+        transform: 'translateX(0px) scale(0.5)'
+      })),
+      transition("default <=> highlighted", animate(300)),
+      transition("highlighted <=> default", animate(800)),
+      transition("shrunken <=>*",[
+        style({
+          'background-color': 'rebeccapurple'
+        }),
+        animate(1000, style({
+          borderRadius: '50px'
+        })),
+        animate(1500)
+      ])
     ])
   ]
 })
 export class AppComponent {
   divAnchor: string = 'default';
+  div2Anchor: string = 'default';
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
@@ -30,5 +55,10 @@ export class AppComponent {
 
     onAnimate(){
       this.divAnchor == 'default' ? this.divAnchor = 'highlighted' : this.divAnchor = 'default';
+      this.div2Anchor == 'default' ? this.div2Anchor = 'highlighted' : this.div2Anchor = 'default';
+    }
+
+    onShrink(){
+      this.div2Anchor = 'shrunken';
     }
 }
